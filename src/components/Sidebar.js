ef6b6b2148eb9409073008c76550c6102b222843
Pallav27 <pallavshrm27@gmail.com>
2024-08-06 18:09:24 +0530

    sidebar changed

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,81 +1,88 @@
-import React from 'react'
-import{
-    MdHomeFilled,
-    MdOutlineSlowMotionVideo,
-    MdSubscriptions,
-    MdOutlineVideoLibrary,
-    MdHistory,
-    MdOutlineWatchLater,
+import React from 'react';
+import {
+  MdHomeFilled,
+  MdOutlineSlowMotionVideo,
+  MdSubscriptions,
+  MdOutlineVideoLibrary,
+  MdHistory,
+  MdOutlineWatchLater,
 } from 'react-icons/md';
-
-import {LuThumbsUp} from 'react-icons/lu';
-
+import { LuThumbsUp } from 'react-icons/lu';
+import { Link } from 'react-router-dom';
 
 export default function Sidebar() {
-    const mainLinks = [
-        {
-            icon: <MdHomeFilled className='text-xl'/>,
-            name: 'Home'
-        },
-        {
-            icon:<MdOutlineSlowMotionVideo className='text-xl'/>,
-            name:'Shorts'
-        },
-        {
-            icon:<MdSubscriptions className='text-xl'/>,
-            name:'Subscriptions'
-        }
-    ];
+  const mainLinks = [
+    {
+      icon: <MdHomeFilled className='text-xl' />,
+      name: 'Home',
+      to: '/home',
+    },
+    {
+      icon: <MdOutlineSlowMotionVideo className='text-xl' />,
+      name: 'Shorts',
+      to: '/shorts',
+    },
+    {
+      icon: <MdSubscriptions className='text-xl' />,
+      name: 'Subscriptions',
+      to: '/subscriptions',
+    },
+  ];
 
-    const otherLinks = [
-        {
-            icon:<MdOutlineVideoLibrary className='text-xl'/>,
-            name:'Library'
-        },
-        {
-            icon:<MdHistory className='text-xl'/>,
-            name:"History"
-        },
-        {
-            icon:<MdOutlineWatchLater className='text-xl'/>,
-            name :"Watch Later"
-        },
-        {
-            icon:<LuThumbsUp className='text-xl'/>,
-            name :"Liked Video"
-        }
-    ]
-
+  const otherLinks = [
+    {
+      icon: <MdOutlineVideoLibrary className='text-xl' />,
+      name: 'Library',
+      to: '/library',
+    },
+    {
+      icon: <MdHistory className='text-xl' />,
+      name: 'History',
+      to: '/history',
+    },
+    {
+      icon: <MdOutlineWatchLater className='text-xl' />,
+      name: 'Watch Later',
+      to: '/watch-later',
+    },
+    {
+      icon: <LuThumbsUp className='text-xl' />,
+      name: 'Liked Video',
+      to: '/liked-videos',
+    },
+  ];
 
   return (
     <div className='w-2/12 bg-[#212121] pr-5 overflow-auto pb-8 sidebar'>
       <ul className='flex flex-col border-b-2 border-gray-700'>
-        {mainLinks.map(
-            ({icon,name}) => {
-            return(
-                <li key={name} className={`pl-6 py-3 hover:bg-zinc-700 ${name === "Home" ?"bg-zinc-600" : " "} rounded-xl`}>
-                    <a href='#' className='flex items-center gap-5'> 
-                    {icon}
-                    <span className='text-sm tracking-wider'>{name}</span>
-                    </a>
-                </li>
-            )}
-        )}
+        {mainLinks.map(({ icon, name, to }) => {
+          return (
+            <li
+              key={name}
+              className={`pl-6 py-3 hover:bg-zinc-700 ${
+                name === 'Home' ? 'bg-zinc-600' : ''
+              } rounded-xl`}
+            >
+              <Link to={to} className='flex items-center gap-5'>
+                {icon}
+                <span className='text-sm tracking-wider'>{name}</span>
+              </Link>
+            </li>
+          );
+        })}
       </ul>
       <ul className='flex flex-col border-b-2 border-gray-800'>
-        {otherLinks.map(
-            ({icon,name}) => {
-            return(
-                <li key={name} className={`pl-6 py-3 hover:bg-zinc-600 `}>
-                    <a href='#' className='flex items-center gap-5'> 
-                    {icon}
-                    <span className='text-sm tracking-wider'>{name}</span>
-                    </a>
-                </li>
-            )
-            }
-        )}
+        {otherLinks.map(({ icon, name, to }) => {
+          return (
+            <li key={name} className='pl-6 py-3 hover:bg-zinc-600'>
+              <Link to={to} className='flex items-center gap-5'>
+                {icon}
+                <span className='text-sm tracking-wider'>{name}</span>
+              </Link>
+            </li>
+          );
+        })}
       </ul>
     </div>
-  )
+  );
 }
